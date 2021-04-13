e0daba01bdb2bd1384655a5f8ed8ab30a9035ca7
Kelly Appleton <kda9036@rit.edu>
2021-04-13 18:01:32 -0400

    Added font import link

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -10,7 +10,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title> <?php echo $pageName;?> </title> 
+        <!-- Stylesheet -->
         <link href="assets/css/styles.css" rel="stylesheet" type="text/CSS">
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">  
     </head>
     <body>
         <div>
@@ -21,16 +25,16 @@
             <nav>
                 <ul>
                     <li ><a href="index.php">Home Page</a></li>
-                    <li ><a href="ch1.php">Chapter 1 - Adding CSS To HTML</a></li>
-                    <li ><a href="ch2.php">Chapter 2 - CSS Selectors</a></li>
-                    <li ><a href="ch3.php">Chapter 3 - CSS Properties</a></li>
-                    <li ><a href="ch4.php">Chapter 4 - Text Properties</a></li>
-                    <li ><a href="ch5.php">Chapter 5 - CSS Layout</a></li>
-                    <li ><a href="ch6.php">Chapter 6 - Width, Height, and Overflow</a></li>
-                    <li ><a href="ch7.php">Chapter 7 - CSS Box Properties</a></li>
-                    <li ><a href="ch8.php">Chapter 8 - Positions</a></li>
-                    <li ><a href="ch9.php">Chapter 9 - Pseudo Classes</a></li>
-                    <li ><a href="ch10.php">Chapter 10 - Order and Specificity</a></li>
+                    <li ><a href="ch1.php">Chapter 1: Adding CSS To HTML</a></li>
+                    <li ><a href="ch2.php">Chapter 2: CSS Selectors</a></li>
+                    <li ><a href="ch3.php">Chapter 3: CSS Properties</a></li>
+                    <li ><a href="ch4.php">Chapter 4: Text Properties</a></li>
+                    <li ><a href="ch5.php">Chapter 5: CSS Layout</a></li>
+                    <li ><a href="ch6.php">Chapter 6: Width, Height, and Overflow</a></li>
+                    <li ><a href="ch7.php">Chapter 7: CSS Box Properties</a></li>
+                    <li ><a href="ch8.php">Chapter 8: Positions</a></li>
+                    <li ><a href="ch9.php">Chapter 9: Pseudo Classes</a></li>
+                    <li ><a href="ch10.php">Chapter 10: Order and Specificity</a></li>
                 </ul>
             </nav>
 
